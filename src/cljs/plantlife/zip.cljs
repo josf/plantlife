49abb22456f0e105495f6a57471860cc2da9445d
Joseph Fahey <fahey.joseph@gmail.com>
2014-12-28 22:08:28 +0100

    use single :children field with a vector
    This lightens considerably the size of the overall graph/zipper.

--- src/cljs/plantlife/zip.cljs
+++ src/cljs/plantlife/zip.cljs
@@ -6,15 +6,14 @@
 (defn plant-zip [tree]
   (zip/zipper
     ;;  branch?: all nodes can have children, but a nil value as child can't
-   (fn [n] (not (nil? n)))             
+    (fn [n] (or (not (nil? n))
+              (not (vector? n))))
     (fn [t]
-      (let [cs (filter identity [(:north-child t) (:south-child t)])]
+      (let [cs (filter identity (:children t))]
         (when (pos? (count cs))
           cs)))
     (fn [nod chils]
-      (-> nod
-        (assoc :north-child (first chils))
-        (assoc :south-child (second chils))))
+      (assoc nod :children (vec (take 2 chils))))
     tree))
 
 

fa514cd9872823cd7cd478607a031b04b861e2fe
Joseph Fahey <fahey.joseph@gmail.com>
2015-01-02 10:20:01 +0100

    components for each branch segment

--- src/cljs/plantlife/views.cljs
+++ src/cljs/plantlife/views.cljs
@@ -9,6 +9,24 @@
 (defn all-branches [zip-tree]
   (map zip/node (take-while (complement zip/end?) (iterate zip/next zip-tree))))
 
+
+(defn branch-view [branch owner]
+  (reify
+    om/IDisplayName
+    (display-name [_] "BranchPath")
+
+    om/IRenderState
+    (render-state [_ state]
+      (let [{:keys [origin-x origin-y dest-x dest-y dest-x-cp dest-y-cp origin-x-cp origin-y-cp depth]} branch]
+        (html
+          [:path {:d  (apply str
+                        (interpose " "
+                          ["M" origin-x origin-y
+                           "C" origin-x-cp origin-y-cp "," dest-x-cp dest-y-cp "," dest-x dest-y]))
+                  :stroke "green" :stroke-width (- 24 (* 4 depth))
+                  :stroke-linecap "round"
+                  :fill "transparent"}])))))
+
 (defn svg-view [branches owner]
   (reify
     om/IDisplayName
@@ -21,4 +39,4 @@
         (vec
          (concat
            [:svg {:version "1.1" :width 1200 :height 800}
-            (map b/branch-section (all-branches (plz/plant-zip branches)))]))))))
+            (om/build-all branch-view (all-branches (plz/plant-zip branches)))]))))))

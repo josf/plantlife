0ec358fa1abad7969672764f251b9680bd2e3cc2
Joseph Fahey <fahey.joseph@gmail.com>
2015-01-06 14:05:08 +0100

    deactivate chestnut react externs
    There is already an externs in a jar somewhere, and this line causes a
    conflict

--- project.clj
+++ project.clj
@@ -32,7 +32,7 @@
                                         :output-dir    "resources/public/js/out"
                                         :source-map    "resources/public/js/out.js.map"
                                         :preamble      ["react/react.min.js"]
-                                        :externs       ["react/externs/react.js"]
+ ;                                       :externs       ["react/externs/react.js"]
                                         :optimizations :none
                                         :pretty-print  true}}
                        :prod {:source-paths ["src/cljs"]
